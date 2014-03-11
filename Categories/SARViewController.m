c510c533bc56b4de0d8da90ed0b4b6c02b4356a8
saravanan.v <saravanan.v@gofrugal.com>
2014-03-11 12:46:08 +0530

    Adding Shake View Effects Sample code

--- Categories/SARViewController.m
+++ Categories/SARViewController.m
@@ -9,7 +9,9 @@
 #import "SARViewController.h"
 #import "UIView+View.h"
 
-@interface SARViewController ()
+@interface SARViewController (){
+    UIView *shakeView;
+}
 
 @end
 
@@ -18,10 +20,22 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
+ 
 
     [self.view addParallaxEffect];
     
+    shakeView = [[UIView alloc]initWithFrame:CGRectMake(100, 200, self.view.frame.size.width-200, self.view.frame.size.height-500)];
+    shakeView.backgroundColor = [UIColor purpleColor];
+    [self.view addSubview:shakeView];
+    
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+    [NSTimer scheduledTimerWithTimeInterval:4.0 target:self selector:@selector(performShakeOperation) userInfo:nil repeats:YES];
+}
+
+-(void)performShakeOperation{
+    [shakeView shakeView:20];
 }
 
 - (void)didReceiveMemoryWarning
